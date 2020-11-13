9f098ee503d3ef4720245f63be3f6ce4463e7420
Albin Médoc <stamline203@gmail.com>
2020-11-13 23:11:37 +0100

    Marker support

---
+++ src/GoogleMapMarker.svelte
@@ -0,0 +1,12 @@
+<script>
+  import { getContext } from 'svelte';
+  import { key } from './contexts.js';
+  const { getMap } = getContext(key);
+  const map = getMap();
+  export let lat;
+  export let lng;
+  const marker = new google.maps.Marker({
+    position: { lat, lng },
+    map: map
+  });
+</script>
--- src/components.js
+++ src/components.js
@@ -3,9 +3,11 @@
 import GoogleSdk from './GoogleSdk.svelte'
 import GoogleMap from './GoogleMap.svelte'
 import GooglePlacesAutocomplete from './GooglePlacesAutocomplete.svelte'
+import GoogleMapMarker from './GoogleMapMarker.svelte'
 
 export {
   GoogleSdk,
   GoogleMap,
-  GooglePlacesAutocomplete
+  GooglePlacesAutocomplete,
+  GoogleMapMarker
 }
